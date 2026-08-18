0a68613366aa31f6fd596ab24ad5cf4cc8563cac
mymcworld3-droid <mymcworld3@gmail.com>
2026-08-18 12:42:47 +0800

    Add player and dummy combat stats

---
+++ js/gameData.js
@@ -0,0 +1,20 @@
+// 玩家基本數值
+const playerStats = {
+    maxHealth: 1145,
+    health: 1145,
+    attack: 66,
+    moveSpeed: 650
+};
+
+// Dummy 基本數值
+const dummyStats = {
+    maxHealth: 1000,
+    health: 1000,
+    defense: 0,
+    moveSpeed: 0
+};
+
+const combatConfig = {
+    attackRange: 150,
+    attackCooldown: 500
+};
